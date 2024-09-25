8beb5ec1d071fefddf84581c4db96239306cd2ad
Amira AbdElsalam <amiraabdelsalam98@yahoo.com>
2024-09-25 11:56:19 +0300

    create output and varibale file for the whole project

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable env {}
+variable avail-zone {}
+variable device-ip {
+  type        = string
+  description = "Enter Your Device IP Address: "
+}
+variable vpc-cidr-block {}
+variable subnet-cidr-block {}
+variable instance-type {}
+variable public-key-loc {}
